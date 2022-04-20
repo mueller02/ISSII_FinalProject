847e8c78e753cc30083fcc98a237b6d9161da5d9
Jason328025 <98444862+Jason328025@users.noreply.github.com>
2022-04-19 21:55:09 -0600

    Update markovConstruction.m

--- markovConstruction.m
+++ markovConstruction.m
@@ -1,5 +1,49 @@
-markovMap = containers.Map('KeyType','sring');
+clear all
+clc
+A = ["Hello"; "Rabbits"];
+B = ["World"];
+%B = [A,B];
 
-markovMap('The') = "tale";
+numOfWords = 10;
 
-markovMap('The')
+markovMap = containers.Map;
+markovMap("keyOne") = [];
+markovMap("keyOne") = [markovMap("keyOne");A];
+markovMap("keyOne") = [markovMap("keyOne");B];
+markovMap("keyOne")
+
+
+fid = fopen('obama.txt');
+data = textscan(fid, '%s');
+fclose(fid);
+stringData = string(data{:});
+stringData;
+
+for i = 1:length(stringData) - 1
+   word = stringData(i);
+   nextWord = stringData(i+1);
+   
+   if(isKey(markovMap,word))
+       markovMap(word) = [markovMap(word); nextWord];
+   else
+       markovMap(word) = [nextWord];
+   end
+   
+end
+
+
+
+randomNum = randi(length(stringData)); %random number bewtween one ans string length
+firstWord = stringData(randomNum);
+outputString = firstWord;
+
+
+for i = 1:numOfWords
+    x = randi(length(markovMap(firstWord)));
+    valArray = markovMap(firstWord);
+    firstWord = valArray(x);
+    outputString = strcat(outputString, " ");
+    outputString = strcat(outputString, firstWord);
+end
+
+outputString
